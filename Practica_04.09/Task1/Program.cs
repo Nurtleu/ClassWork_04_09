35c21b7f975d1d84f1b464affb3b0d3bcff0e0ac
Nurtleu <nur71701@gmail.com>
2017-09-04 18:55:45 +0600

    __04.09.2017__

---
+++ Practica_04.09/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace Task1
+{
+    class Program
+    {
+        //1.	Написать программу, читающую побайтно заданный файл и подсчитывающую число появлений каждого из 256 возможных знаков.
+        static void Main(string[] args)
+        {
+            using (FileStream stream =new FileStream(@"C:\data\1.txt",FileMode.OpenOrCreate))
+            {
+                byte[] bytes = new byte[stream.Length];
+                //stream.Write(bytes, 0, bytes.Length);
+                string symbol = System.Text.Encoding.Unicode.GetString(bytes);
+                int[] signs = new int[256];
+
+                for (int i=0; i< symbol.Length; i++)
+                {
+                    signs[symbol[i]]++;
+                }
+                OutputEncoding = System.Text.Encoding.Unicode;
+                WriteLine("символ - количество");
+                for (int i=0; i<256; i++)
+                {
+                    WriteLine((char)i + "=" + signs[i]);                    
+                }
+                ReadLine();
+            }            
+        }
+    }
+}
---
+++ Practica_04.09/task2(1)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Console;
+
+namespace task2_1_
+{
+    //2.	С помощью класса StreamWriter записать в текстовый файл свое имя, фамилию и возраст. Каждая запись должна начинаться с новой строки.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string writePath = @"C:\data\2.txt";
+
+            string name = "Nurtleu";
+            string surname = "Omirzhanov";
+            int age = 16;
+
+            using (StreamWriter stream = new StreamWriter(writePath, true, System.Text.Encoding.Unicode))
+            {   
+                stream.WriteLine(name);
+                stream.WriteLine(surname);
+                stream.WriteLine(age);
+            };
+        }
+    }
+}
